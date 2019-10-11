30b17abcdb6b866ee9b0970831906c5da91487ba
Luis Cavalari <lfelipe.felipe4@gmail.com>
2019-10-11 00:17:06 -0300

    other minor changes

--- __tests__/unit/user.test.js
+++
@@ -1,7 +0,0 @@
-const factory = require('../factory')
-describe('User',()=>{
-    
-    it('Should authenticate a user',()=>{
-        const user = await factory.create("User");
-    })
-})
---
+++ src/app/Validators/UserCreate.js
@@ -0,0 +1,28 @@
+const Yup = require('yup')
+
+module.exports = async (request, response, next) => {
+    const schema = Yup.object.shape({
+        email: Yup
+            .string()
+            .email('Invalid email')
+            .required('Please inform a email'),
+        password: Yup
+            .string()
+            .required('Please inform a password')
+            .min(10, 'Password need a minimum of 4 characters'),
+        name: Yup
+            .string()
+            .required('Please inform a name')
+
+
+    })
+
+    try {
+        await schema.validate(request.body)
+
+        return next()
+        
+    } catch (error) {
+        return response.status(400).send({ message: error.erros })
+    }
+}
