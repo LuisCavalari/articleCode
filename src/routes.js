859ccb52419fc0ca4f788db72773f3eaf72b6f9b
Luis Cavalari <lfelipe.felipe4@gmail.com>
2019-10-11 00:09:12 -0300

    add auth routes and auth middleware

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
+const auth = require('./app/middlewares/auth')
+const UserCreateValidator = require('./app/Validators/UserCreate') 
 const UserController = require('./app/Controllers/UserController')
+const SessionControler = require('./app/Controllers/SessionController')
 
-router.post('/user', UserController.store)
+router.post('/user', UserCreateValidator , UserController.store)
+router.post('/session', SessionControler.store)
+// private routes bellow
+router.use(auth)
 
 module.exports = router
